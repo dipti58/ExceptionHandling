bc123d0f9fb70f7a240ad7c1b49a3e0cd2d43354
dipti58 <biswaldipti757@gmail.com>
2025-11-23 21:57:45 +0530

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,13 @@
+public class test3 {
+    public static void main(String[] args) {
+        try{
+        System.out.println(10/0);
+        }
+        catch(ArithmeticException e){
+            e.printStackTrace();
+            System.out.println(e);
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+        }
+    }
+}
